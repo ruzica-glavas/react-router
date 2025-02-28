cb4736758c44396fa59d52ae75d4e85a4f8e385c
ruzica-glavas <ruzi99@hotmail.it>
2025-02-28 15:28:13 +0100

    add: bootstrap navbar

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -4,12 +4,44 @@ const MainNav = () => {
     return (
 
         <>
-        <nav>
-            <NavLink to="/">HomePage</NavLink>
-            <NavLink to="/info">Chi siamo</NavLink>
-            <NavLink to="/list">Lista Posts</NavLink>
-        </nav>
-        
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            Navbar
+          </NavLink>
+          <button
+            className="navbar-toggler d-lg-none"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavId">
+            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link" aria-current="page">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/info" className="nav-link" aria-current="page">
+                  Chi siamo
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/list" className="nav-link" aria-current="page">
+                  Lista Posts
+                </NavLink>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
 
         </>
         
