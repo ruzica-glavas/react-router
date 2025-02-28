0c1485f7ca2a227ed3d6bf8840fbdc64c0efad1c
ruzica-glavas <ruzi99@hotmail.it>
2025-02-28 12:19:19 +0100

    add: import pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+//import per le pagine
+import HomePage from "./pages/HomePage"
+import ChiSiamo from "./pages/ChiSiamo"
+import ListaPost from "./pages/ListaPost"
 
 function App() {
 
   return (
     <>
-     
+     <HomePage></HomePage>
+     <ChiSiamo></ChiSiamo>
+     <ListaPost></ListaPost>
     </>
   )
 }
