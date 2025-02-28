6d5630139b30a118f982d5cccb60f58aca4f0e11
ruzica-glavas <ruzi99@hotmail.it>
2025-02-28 15:35:35 +0100

    add: fontawesome and change "home" with an icon

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -7,7 +7,7 @@ const MainNav = () => {
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
-            Navbar
+          <i className="fa-solid fa-house"></i>
           </NavLink>
           <button
             className="navbar-toggler d-lg-none"
@@ -22,11 +22,11 @@ const MainNav = () => {
           </button>
           <div className="collapse navbar-collapse" id="collapsibleNavId">
             <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-              <li className="nav-item">
+              {/*<li className="nav-item">
                 <NavLink to="/" className="nav-link" aria-current="page">
                   Home
                 </NavLink>
-              </li>
+              </li> */}
               <li className="nav-item">
                 <NavLink to="/info" className="nav-link" aria-current="page">
                   Chi siamo
