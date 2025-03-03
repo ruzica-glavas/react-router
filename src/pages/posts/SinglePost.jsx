7b35e918ec95ad2fd0fec3ab0257a8773726be14
ruzica-glavas <ruzi99@hotmail.it>
2025-03-03 17:07:10 +0100

    add: comment for Number()

--- src/pages/posts/SinglePost.jsx
+++ src/pages/posts/SinglePost.jsx
@@ -26,7 +26,7 @@ const SinglePost = () =>{
             Torna indietro
         </button>
 
-        <button className="btn btn-primary" onClick={() => navigate (`/list/${Number(id)+1}`)}>
+        <button className="btn btn-primary" onClick={() => navigate (`/list/${Number(id)+1}`)}> {/* Avrei potuto usare anche parseInt() */}
             Vai avanti
         </button>
         
