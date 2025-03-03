46555509ea92b3f6c613cb93a649704790cdfbcb
ruzica-glavas <ruzi99@hotmail.it>
2025-03-03 12:54:55 +0100

    add: axios and map function

--- src/pages/posts/SinglePost.jsx
+++ src/pages/posts/SinglePost.jsx
@@ -1,12 +1,41 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const SinglePost = () =>{
     
     const {id} = useParams()
+
+    const [post, setPost] = useState ({
+        id: ``,
+        title: ``,
+        content: ``,
+        image: ``,
+        tags: []
+    })
+
+    useEffect (()=>{
+        axios
+        .get (`http://localhost:3000/posts/${id}`)
+        .then ((res) => setPost(res.data))
+        .catch (err => console.error(err));
+    }, [id])
     return(
         <>
         ID prodotto : {id}
+        Titolo: {post.title}
+
+       <ul>
+        {
+           post.content.map( (element, id) => {
+            return(
+                <li key={id}> {element} </li>
+            )
+        }  )
+        }
+       </ul>
+       
+        
         </>
     )
 }
